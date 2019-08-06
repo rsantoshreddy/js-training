eae1a61c24da2baf338a1764b8073ced28b27a52
rsantoshreddy <rsantoshreddy09@gmail.com>
2019-08-06 23:20:22 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+﻿// console.log('We are ready to learn');
+// console.log(document.getElementById('heading').textContent);
+
+/* var Name = 'xyz';
+var name = 'mnp';
+var x = 10;
+var y;
+*/
+
+console.log(name, Name, x, y);
+// var 98Year = 10
+// console.log(98Year);
+var year2019 = 2019;
