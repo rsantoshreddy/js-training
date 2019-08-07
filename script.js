e51ac7206fd2dfa3a45fad8c0f30d56938c96357
rsantoshreddy <rsantoshreddy09@gmail.com>
2019-08-07 23:28:41 +0530

    Day 2 changes

--- script.js
+++ script.js
@@ -7,7 +7,96 @@ var x = 10;
 var y;
 */
 
-console.log(name, Name, x, y);
+// console.log(name, Name, x, y);
 // var 98Year = 10
 // console.log(98Year);
-var year2019 = 2019;
+// var year2019 = 2019;
+
+// Basic data types (Primary data types);
+// var name = 'Teja'; //string
+// var age = 20; //Number
+// var isMarried = false; // Boolean
+// var taluka = 'tara stana'; // undefined
+
+// //  loosely typed
+// name = 'Mahesh';
+// name = 1230;
+// name = true;
+
+// console.log('Name =', name, 'Mister');
+// console.log('age = ', age, 'Young');
+// console.log('isMarried = ', isMarried, 'Happy family');
+
+// var increasedAge = 5;
+// age = age + increasedAge;
+// console.log('age = ', age, 'Young');
+
+// var village;
+// console.log(village); // undefined
+// console.log(postal); //error
+
+// age + increasedAge
+// age = operand
+// increasedAge = operand
+// + = addition operator
+// = assignment operator
+
+// Operators
+// Unary operator (taking only one operand)
+// ++
+// --
+// !
+
+// Binary operator (takes two operand)
+
+// ++/--
+var myScore = 10;
+console.log(++myScore); // myScore = myScore + 1;
+console.log(--myScore); // myScore = myScore - 1;
+console.log(myScore++); // myScore; myScore = myScore + 1;
+console.log(myScore);
+
+var aNumber = 0;
+console.log(++aNumber);
+console.log(--aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+console.log(++aNumber);
+
+var myVariable = 20;
+var yourVarialbe = 0;
+
+// 0/undefined : false
+// anything : true
+
+var married = true;
+console.log('married', married);
+console.log('!married', !married);
+console.log('!myVariable', !myVariable);
+console.log('!yourVarialbe', !yourVarialbe);
+
+var score1 = 100,
+  score2 = 200;
+
+score2 += score1; // score2 =  score2 + score1; // 300
+score2 -= score1; // score2 = score2 - score1; 300 - 100 = 200
+console.log(score2);
+
+// Multiplication *
+// Divide /
+// Add +
+// Minus -
+
+console.log('2 * 3 =', 2 * 3);
+console.log('10 / 2 =', 10 / 2);
+console.log('10 - 20 =', 10 - 20);
+console.log('10 + 20 =', 10 + 20);
+
+// alert('youafd afaj fdalkjfopquefwpojasf aybfposahfp9qyrfoah 2pyr awpof hpaso fhwa');
+
+var myNUmber = (1 + 3) * 2; //4*2=8
+console.log(myNUmber);
